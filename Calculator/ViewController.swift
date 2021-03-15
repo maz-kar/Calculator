ebbd24fe78d0c486361646bd8fdd72badcc85d56
Maziar Layeghkar <maziar.layeghkar@icloud.com>
2021-03-15 13:26:38 +0100

    Add if let for Optional

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  Calculator
-//
-//  Created by Angela Yu on 10/09/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
-//
 
 import UIKit
 
@@ -12,19 +5,17 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
-    
-    
     @IBAction func calcButtonPressed(_ sender: UIButton) {
-        
-        //What should happen when a non-number button is pressed
     
     }
-
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
-        //What should happen when a number is entered into the keypad
-    
+        //numButtonPressed: num shown on displayLabel
+        
+        if let numValue = sender.currentTitle{
+            displayLabel.text = numValue
+        }
     }
 
 }
