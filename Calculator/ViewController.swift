956ba524aaf511f45c3e7edb602960e4682daa36
Maziar Layeghkar <maziar.layeghkar@icloud.com>
2021-03-15 13:43:58 +0100

    Add prop isFinishedTypingNumber to accumulate numbers

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -5,18 +5,27 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
+    var isFinishedTypingNumber: Bool = true
+    
     @IBAction func calcButtonPressed(_ sender: UIButton) {
     
     }
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
-        
-        //numButtonPressed: num shown on displayLabel
-        
-        if let numValue = sender.currentTitle{
-            displayLabel.text = numValue
-        }
-    }
 
+        if let numValue = sender.currentTitle{
+            
+            if isFinishedTypingNumber { //display shows 0 or is ready to get new num
+                displayLabel.text = numValue //e.g: 1
+                isFinishedTypingNumber = false // let it continue typing
+            } else {
+                //if this else triggered, add to previous num in the displayLabel + newValue (numValue)
+                displayLabel.text = displayLabel.text! + numValue
+            }
+                
+        }
+            
+    }
 }
 
+//A property needed that keeps hold of whether if the user is done typing the number
