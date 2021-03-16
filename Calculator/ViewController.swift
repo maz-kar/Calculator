375cc426c136b6ba318f85a318b4324d43617f48
Maziar Layeghkar <maziar.layeghkar@icloud.com>
2021-03-16 11:57:42 +0100

    Add getter

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -7,31 +7,40 @@ class ViewController: UIViewController {
     
     var isFinishedTypingNumber: Bool = true
     
+    //as the displayLabel.text is a String the getter that we want to use should be sth els like Double
+    //for future use whenever someone want an access to the displayValue is useful
+    //what it does: it will try to convert what currently is inside the displayLabel.text into a Double and it fails, it will give a fatal error BUT if succeed it will return the number as the value of the displayValue
+    var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display label text to a Double.")
+            }
+            return number
+        }
+    }
+    
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         isFinishedTypingNumber = true
         
-        guard let number = Double(displayLabel.text!) else {
-            fatalError("Cannot convert display label text to a Double.")
-        }
+
         
         if let calcMethod = sender.currentTitle {
             if calcMethod == "AC" {
                 displayLabel.text = "0"
             }
             else if calcMethod == "+/-" {
-                displayLabel.text = String(Int(number * -1))
+                displayLabel.text = String(displayValue * -1)
             }
             else if calcMethod == "%" {
-                displayLabel.text = String(number / 100)
+                displayLabel.text = String(displayValue / 100)
             }
         }
     
     }
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
-        
-        if let numValue = sender.currentTitle { //numValue is one of 0-9
-            if isFinishedTypingNumber {
+        if let numValue = sender.currentTitle {
+            if isFinishedTypingNumber {
                 if numValue == "." {
                     displayLabel.text = "0."
                 }
@@ -40,13 +49,13 @@ class ViewController: UIViewController {
                 }
                 isFinishedTypingNumber = false
             } else {
-                if numValue == "." && displayLabel.text!.contains("."){
+                if numValue == "." && displayLabel.text!.contains(".") {
                     return
                 }
-                displayLabel.text = displayLabel.text! + numValue
+                else {
+                    displayLabel.text! += numValue
+                }
             }
-            
-            
         }
             
     }
