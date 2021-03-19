d77e806f2764885f947793b48a5f36362538983e
Maziar Layeghkar <maziar.layeghkar@icloud.com>
2021-03-19 11:17:35 +0100

    Does performTwoNumberCalculation

--- Calculator/Model/CalculatorLogic.swift
+++ Calculator/Model/CalculatorLogic.swift
@@ -12,7 +12,7 @@ struct CalculatorLogic {
     }
     
     mutating func calculate(symbol: String) ->Double? {
-    
+        
         if let n = number {
             
             if symbol == "AC" {
@@ -24,8 +24,22 @@ struct CalculatorLogic {
             else if symbol == "%" {
                 return n * 0.01
             }
-            else if symbol == "+" {
+            else if symbol == "=" {
+                return perfomTwoNumCalculation(n2: n)
+            }
+            else {
                 intermediateCalculation = (n1: n, calcMethod: symbol)
+            }
+        }
+        return nil
+    }
+    
+    private func perfomTwoNumCalculation(n2: Double) -> Double? {
+        
+        if let n1 = intermediateCalculation?.n1, let operation = intermediateCalculation?.calcMethod {
+            
+            if operation == "+" {
+                return n1 + n2
             }
             
         }
