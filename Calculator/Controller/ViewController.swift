4ee6361bbbdf0e2ec7835d6e8ce0cecc34b4c373
Maziar Layeghkar <maziar.layeghkar@icloud.com>
2021-03-24 10:41:51 +0100

    Add comments

--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -3,11 +3,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet weak var displayLabel: UILabel! //displaylabel on top
     
-    var isFinishedTypingNumber: Bool = true
+    private var isFinishedTypingNumber: Bool = true //if true means we just started to type
     
-    var displayValue: Double {
+    private var calculator = CalculatorLogic() //object from Model
+    
+    private var displayValue: Double { //to prevent casting all over the place
+        
         get {
             guard let number = Double(displayLabel.text!) else {
                 fatalError("Cannot convert display label text to a Double.")
@@ -19,24 +22,24 @@ class ViewController: UIViewController {
         }
     }
     
-    private var calculator = CalculatorLogic()
-    
-    @IBAction func numButtonPressed(_ sender: UIButton) {
-        if let numValue = sender.currentTitle {
-            if isFinishedTypingNumber {
-                if numValue == "." {
-                    displayLabel.text = "0."
+    @IBAction func numButtonPressed(_ sender: UIButton) { //when 0-9 was pressed
+        if let numValue = sender.currentTitle { //to catch the pressed number
+            
+            if isFinishedTypingNumber { //Just started to type
+                if numValue == "." { //if the pressed value started with .
+                    displayLabel.text = "0." //change the display to 0.
                 }
                 else {
-                    displayLabel.text = numValue
+                    displayLabel.text = numValue //if the pressed num is not starting with "." show the num on displayLabel
                 }
-                isFinishedTypingNumber = false
-            } else {
-                if numValue == "." && displayLabel.text!.contains(".") {
-                    return
+                isFinishedTypingNumber = false //continue accepting digits
+            }
+            else { //case: if isFinishedTypingNumber == false
+                if numValue == "." && displayLabel.text!.contains(".") { //To cever both cases of starting with "." and case of 2.3
+                    return //Don't let any extra "."
                 }
                 else {
-                    displayLabel.text! += numValue
+                    displayLabel.text! += numValue //
                 }
             }
         }
--- Calculator/README.swift
+++ Calculator/README.swift
@@ -1,3 +1,2 @@
-//isFinishedTypingNumber true: press is a display reset for getting new digits
-//isFinishedTypingNumber false: we can still type new digits sequentially
-//
+
+//It's a calculator app that does: "AC", "+/-", "%", "+", "-", "*", "/"
